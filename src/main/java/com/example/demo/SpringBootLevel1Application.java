06db1891e7f5a1f0b14132e0daf7bd892846b362
yuri sato <sato@yurinoMacBook-Air.local>
2023-02-16 16:14:45 +0900

    First Commit

---
+++ src/main/java/com/example/demo/SpringBootLevel1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLevel1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLevel1Application.class, args);
+	}
+
+}
